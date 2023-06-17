89bd2573a1fdc0d1c019202bb3d0ae116149078a
Alejandro <aobregonrodri@gmail.com>
2023-06-17 22:51:10 +0200

    ok

--- src/components/parametros/Parametros.jsx
+++ src/components/parametros/Parametros.jsx
@@ -1,16 +1,39 @@
 import { MaterialReactTable } from "material-react-table"
 import { useFetchParametrosQuery } from "../../features/empresa-api-slice"
 import { generateColumns, generateColumns2 } from "../../helpers/gHeaderColumns"
+import { Space, Tag } from "antd"
+import { useState } from "react"
 
 
 export const Parametros = () => {
 
     const { isLoading, data = [] } = useFetchParametrosQuery()
-    const columns = generateColumns2(data)
+    const [selected, setSeleted] = useState('')
+    const {CheckableTag}= Tag
 
+    function handleSelect(e) {
+        // console.log(e)
+        setSeleted(e.target.id)
+    }
+    const columns = !data[selected]?.length ? [] : generateColumns2(data[selected])
+    console.log(data)
     return (
-        <div className="w-full">
-            {<MaterialReactTable columns={columns} id='s' data={data} />}
+        <div className="w-full flex flex-col">
+            <Space wrap>
+                {
+                    Object.keys(data).map((item, idx) => (
+                        <CheckableTag key={idx + item} id={item} checked={item ===selected } onClick={handleSelect} >
+                            {item}
+                        </CheckableTag>
+                    ))
+                }
+            </Space>
+            <p>{selected}</p>
+            <Space>
+                {
+                    !data[selected]?.length ? <></> : <MaterialReactTable columns={columns} data={data[selected]} />
+                }
+            </Space>
         </div>
     )
 }
